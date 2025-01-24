0921ba4ddfcec4b15cf6ae1c51f21a388934b3fa
hmacdope <hugomacdermott@gmail.com>
2025-01-24 12:45:24 +1100

    get most basic anvil test working

--- openadmet_models/tests/anvil/test_anvil.py
+++ openadmet_models/tests/anvil/test_anvil.py
@@ -1,6 +1,12 @@
+import pytest
 from openadmet_models.anvil.anvil_workflow import AnvilWorkflow
+from openadmet_models.tests.datafiles import basic_anvil_yaml
+
 
 
 
 def test_anvil_workflow_create():
-    
+    anvil_workflow = AnvilWorkflow.from_yaml(basic_anvil_yaml)
+
+
+
--- openadmet_models/tests/datafiles.py
+++ openadmet_models/tests/datafiles.py
@@ -1,9 +1,9 @@
 from importlib import resources
-import openadmet_models.test_data
+import openadmet_models.tests.test_data
 
-_data_ref = resources.files("openadmet_models.test_data")
+_data_ref = resources.files("openadmet_models.tests.test_data")
 
 
-anvil_yaml = (
+basic_anvil_yaml = (
     _data_ref / "basic_anvil.yaml"
 ).as_posix()
