e61386d1293746ca4bc121a6fdbe6aace6df5ad6
hmacdope <hugomacdermott@gmail.com>
2025-02-14 15:10:02 +1100

    improve cross validation

--- openadmet_models/eval/cross_validate.py
+++ openadmet_models/eval/cross_validate.py
@@ -1,8 +1,11 @@
-from sklearn.model_selection import KFold, cross_val_score, RepeatedKFold
+from sklearn.model_selection import KFold, cross_validate, RepeatedKFold
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from openadmet_models.eval.eval_base import EvalBase, evaluators
 from openadmet_models.eval.regression import stat_and_bootstrap, nan_omit_ktau, nan_omit_spearmanr
-
+from scipy.stats import bootstrap, kendalltau, spearmanr
+from sklearn.metrics import make_scorer
+import json
+from loguru import logger
 
 @evaluators.register("SKLearnRepeatedKFoldCrossValidation")
 class SKLearnRepeatedKFoldCrossValidation(EvalBase):
@@ -20,27 +23,27 @@ class SKLearnRepeatedKFoldCrossValidation(EvalBase):
         if model is None or X_train is None or y_train is None:
             raise ValueError("model, X_train, and y_train must be provided")
 
-        # tuple of metric, whether it is a scipy statistic, and the name to use in the report
+        # store the metric names and callables in dict suitable for sklearn cross_validate
         self.metrics = {
-            "mse": (mean_squared_error, False, "MSE"),
-            "mae": (mean_absolute_error, False, "MAE"),
-            "r2": (r2_score, False, "$R^2$"),
-            "ktau": (nan_omit_ktau, True, "Kendall's $\\tau$"),
-            "spearmanr": (nan_omit_spearmanr, True, "Spearman's $\\rho$"),
+            "mse": make_scorer(mean_squared_error),
+            "mae": make_scorer(mean_absolute_error),
+            "r2": make_scorer(r2_score),
+            "ktau": make_scorer(kendalltau, nan_policy="omit"),
+            "spearmanr": make_scorer(spearmanr, nan_policy="omit")
         }
 
-        # store the metric names and callables in dict suitable for sklearn cross_val_score
-        self.metric_map = {k: v[0] for k, v in self.metrics.items()}
-        print(self.metric_map)
-
-        # create a cross-validation object
+        logger.info("Starting cross-validation")
+        
+        # run CV
         cv = RepeatedKFold(
             n_splits=self.n_splits, n_repeats=self.n_repeats, random_state=self.random_state
         )
 
         estimator = model.model
         # evaluate the model,
-        scores = cross_val_score(estimator, X_train, y_train, cv=cv, n_jobs=-1, scoring = self.metric_map)
+        scores = cross_validate(estimator, X_train, y_train, cv=cv, n_jobs=-1, scoring = self.metrics)
+
+        logger.info("Cross-validation complete")
 
         print(scores)
         raise Exception("stop")
