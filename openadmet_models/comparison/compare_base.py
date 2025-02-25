dd08a49f02b2bb096ee32922708c0b55b9b8b6b0
Devany West <dwest@rockefeller.edu>
2025-02-25 15:50:59 -0500

    Adding comparison classes

---
+++ openadmet_models/comparison/compare_base.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+from class_registry import ClassRegistry, RegistryKeyError
+from pydantic import BaseModel
+
+comparisons = ClassRegistry(unique=True)
+
+def get_comparison_class(compare_type):
+    try:
+        compare_class = comparisons.get_class(compare_type)
+    except RegistryKeyError:
+        raise ValueError(f"Compariosn type {compare_type} not found in comparisons catalouge")
+    return compare_class
+
+class ComparisonBase(BaseModel, ABC):
+
+    @abstractmethod
+    def compare():
+        pass
