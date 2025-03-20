ddd53fa27739fc5e44769f2b380998ed489c9299
Devany West <devanywest314@gmail.com>
2025-03-20 15:36:12 -0400

    Fixing test errors

--- openadmet_models/tests/comparison/test_comparison.py
+++ openadmet_models/tests/comparison/test_comparison.py
@@ -16,7 +16,7 @@ def test_posthoc_comparison():
     model_tags = ["descr", "fp", "combined"]
     comp_obj = PostHocComparison()
     levene, tukeys_df = comp_obj.compare(model_stats, model_tags)
-    assert levene["mse"][0].statistic == 0.29637389987684526
-    assert levene["ktau"][0].statistic == 0.05033310952264555
+    assert levene["mse"][0] == 0.29637389987684526
+    assert levene["ktau"][0] == 0.05033310952264555
     assert tukeys_df["metric_val"][0] == 0.00705013739584795
     assert tukeys_df["pvalue"][14] == 1.600273813462394e-08
