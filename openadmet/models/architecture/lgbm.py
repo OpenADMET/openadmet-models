4adbb513f023058f6d832b9fbfbbc1d0756d5fd7
Sean Colby <seanmcolby@gmail.com>
2025-03-26 10:36:51 -0800

    Reduce redundancy by introducing an LGBM base model class

--- openadmet/models/architecture/lgbm.py
+++ openadmet/models/architecture/lgbm.py
@@ -1,4 +1,4 @@
-from typing import ClassVar
+from typing import ClassVar, Type
 
 import lightgbm as lgb
 import numpy as np
@@ -7,88 +7,66 @@ from loguru import logger
 from openadmet.models.architecture.model_base import PickleableModelBase, models
 
 
+class LGBMModelBase(PickleableModelBase):
+    """
+    Base class for LightGBM models
+    """
+
+    type: ClassVar[str]
+    model_class: ClassVar[
+        Type
+    ]  # To specify the LightGBM model class (e.g., LGBMRegressor or LGBMClassifier)
+    model_params: dict = {}
+
+    @classmethod
+    def from_params(cls, class_params: dict = {}, model_params: dict = {}):
+        """
+        Create a model from parameters
+        """
+        instance = cls(**class_params, model_params=model_params)
+        instance.build()
+        return instance
+
+    def train(self, X: np.ndarray, y: np.ndarray):
+        """
+        Train the model
+        """
+        self.build()
+        self.model = self.model.fit(X, y)
+
+    def build(self):
+        """
+        Prepare the model
+        """
+        if not self.model:
+            self.model = self.model_class(**self.model_params)
+        else:
+            logger.warning("Model already exists, skipping build")
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Predict using the model
+        """
+        if not self.model:
+            raise ValueError("Model not trained")
+        return self.model.predict(X)
+
+
 @models.register("LGBMRegressorModel")
-class LGBMRegressorModel(PickleableModelBase):
+class LGBMRegressorModel(LGBMModelBase):
     """
     LightGBM regression model
     """
 
     type: ClassVar[str] = "LGBMRegressorModel"
-    model_params: dict = {}
-
-    @classmethod
-    def from_params(cls, class_params: dict = {}, model_params: dict = {}):
-        """
-        Create a model from parameters
-        """
-
-        instance = cls(**class_params, model_params=model_params)
-        instance.build()
-        return instance
-
-    def train(self, X: np.ndarray, y: np.ndarray):
-        """
-        Train the model
-        """
-        self.build()
-        self.model = self.model.fit(X, y)
-
-    def build(self):
-        """
-        Prepare the model
-        """
-        if not self.model:
-            self.model = lgb.LGBMRegressor(**self.model_params)
-        else:
-            logger.warning("Model already exists, skipping build")
-
-    def predict(self, X: np.ndarray) -> np.ndarray:
-        """
-        Predict using the model
-        """
-        if not self.model:
-            raise ValueError("Model not trained")
-        return self.model.predict(X)
+    model_class: ClassVar[Type] = lgb.LGBMRegressor
 
 
 @models.register("LGBMClassifierModel")
-class LGBMClassifierModel(PickleableModelBase):
+class LGBMClassifierModel(LGBMModelBase):
     """
     LightGBM classification model
     """
 
     type: ClassVar[str] = "LGBMClassifierModel"
-    model_params: dict = {}
-
-    @classmethod
-    def from_params(cls, class_params: dict = {}, model_params: dict = {}):
-        """
-        Create a model from parameters
-        """
-        instance = cls(**class_params, model_params=model_params)
-        instance.build()
-        return instance
-
-    def train(self, X: np.ndarray, y: np.ndarray):
-        """
-        Train the model
-        """
-        self.build()
-        self.model = self.model.fit(X, y)
-
-    def build(self):
-        """
-        Prepare the model
-        """
-        if not self.model:
-            self.model = lgb.LGBMClassifier(**self.model_params)
-        else:
-            logger.warning("Model already exists, skipping build")
-
-    def predict(self, X: np.ndarray) -> np.ndarray:
-        """
-        Predict using the model
-        """
-        if not self.model:
-            raise ValueError("Model not trained")
-        return self.model.predict(X)
+    model_class: ClassVar[Type] = lgb.LGBMClassifier
