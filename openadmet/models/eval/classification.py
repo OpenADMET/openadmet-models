79a43821741d07c228fc84f83be368ecf9ecbf06
Sean Colby <seanmcolby@gmail.com>
2025-03-27 08:30:41 -0800

    Add initial plotting functionality

--- openadmet/models/eval/classification.py
+++ openadmet/models/eval/classification.py
@@ -7,13 +7,24 @@ from pydantic import Field
 from scipy.stats import bootstrap
 from sklearn.metrics import (
     accuracy_score,
+    auc,
     f1_score,
+    precision_recall_curve,
     precision_score,
     recall_score,
     roc_auc_score,
+    roc_curve,
 )
 
 from openadmet.models.eval.eval_base import EvalBase, evaluators
+
+
+def pr_auc_score(y_true, y_pred):
+    """
+    Calculate the area under the precision-recall curve
+    """
+    precision, recall, _ = precision_recall_curve(y_true, y_pred)
+    return auc(recall, precision)
 
 
 @evaluators.register("ClassificationMetrics")
@@ -31,6 +42,7 @@ class ClassificationMetrics(EvalBase):
         "recall": (recall_score, False, "Recall"),
         "f1": (f1_score, False, "F1 Score"),
         "roc_auc": (roc_auc_score, False, "ROC AUC"),
+        "pr_auc": (pr_auc_score, False, "PR AUC"),
     }
 
     def evaluate(
@@ -175,3 +187,93 @@ class ClassificationMetrics(EvalBase):
             stat_caption += f"{self._metrics[metric][2]}: {value:.2f}$_{{{lower_ci:.2f}}}^{{{upper_ci:.2f}}}$\n"
         stat_caption += f"Confidence level: {confidence_level}"
         return stat_caption
+
+
+@evaluators.register("ClassificationPlots")
+class ClassificationPlots(EvalBase):
+    axes_labels: list[str] = Field(
+        ["Measured", "Predicted"], description="Labels for the axes"
+    )
+    title: str = Field("Pred vs ", description="Title for the plot")
+    plots: dict = {}
+    use_wandb: bool = Field(False, description="Whether to use wandb")
+    dpi: int = Field(300, description="DPI for the plot")
+
+    def evaluate(self, y_true=None, y_pred=None, use_wandb=False, **kwargs):
+        """
+        Evaluate the classification model
+        """
+        if use_wandb:
+            self.use_wandb = use_wandb
+
+        if y_true is None or y_pred is None:
+            raise ValueError("Must provide y_true and y_pred")
+
+        self.plots = {
+            "auroc": self.auroc,
+            "aupr": self.aupr,
+        }
+
+        self.plot_data = {}
+
+        # if self.do_stats:
+        #     rm = ClassificationMetrics()
+        #     rm.evaluate(y_true, y_pred)
+        #     stat_caption = rm.make_stat_caption()
+
+        # create the plots
+        for plot_tag, plot in self.plots.items():
+            self.plot_data[plot_tag] = plot(
+                y_true,
+                y_pred,
+                xlabel=self.axes_labels[0],
+                ylabel=self.axes_labels[1],
+                title=self.title,
+                stat_caption=stat_caption,
+                pXC50=self.pXC50,
+                min_val=self.min_val,
+                max_val=self.max_val,
+            )
+
+    @staticmethod
+    def auroc(
+        y_true,
+        y_pred,
+        xlabel="Measured",
+        ylabel="Predicted",
+        title="",
+        stat_caption="",
+    ):
+        fpr, tpr, _ = roc_curve(y_true, y_pred)
+        pass
+
+    @staticmethod
+    def aupr(
+        y_true,
+        y_pred,
+        xlabel="Measured",
+        ylabel="Predicted",
+        title="",
+        stat_caption="",
+    ):
+        precision, recall, _ = precision_recall_curve(y_true, y_pred)
+        pass
+
+    def report(self, write=False, output_dir=None):
+        """
+        Report the evaluation
+        """
+        if write:
+            self.write_report(output_dir)
+        return self.plot_data
+
+    def write_report(self, output_dir):
+        """
+        Write the evaluation report
+        """
+
+        for plot_tag, plot in self.plot_data.items():
+            plot_path = output_dir / f"{plot_tag}.png"
+            plot.savefig(plot_path, dpi=self.dpi)
+            if self.use_wandb:
+                wandb.log({plot_tag: wandb.Image(str(plot_path))})
