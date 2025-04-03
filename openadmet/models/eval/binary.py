67070f8b0f505ded8219ed1b0feb5176a1a8659c
Devany West <devanywest314@gmail.com>
2025-04-03 11:52:02 -0400

    Small fixes from Hugo: fixed docstrings, plot title, etc

--- openadmet/models/eval/binary.py
+++ openadmet/models/eval/binary.py
@@ -17,41 +17,62 @@ class PosthocBinaryMetrics(EvalBase):
     Not intended for binary models
     """
 
-    def evaluate(self, y_true:list = None, y_pred:list = None, cutoff:float = None, report:bool = False, output_dir:str = None):
+    def evaluate(self, y_true: list = None, y_pred: list = None, cutoff: float = None, report: bool = False, output_dir: str = None):
         """
-        Evaluate the precision and recall metrics for model with user-input cutoffs.
+        Evaluate the precision and recall metrics for the model with user-input cutoffs.
 
-        Parameters:
-        y_true (array-like, optional): True values.
-        y_pred (array-like, optional): Predicted values.
-        cutoffs (list, optional): List of cutoff values to calculate precision and recall.
-        report (bool, optional): Whether to save jsons of resulting precision/recall metrics. Default is False.
-        output_dir (str, optional): Directory to save the output plots and report. Default is None.
+        Parameters
+        ----------
+        y_true : array-like
+            True values or labels.
+        y_pred : array-like
+            Predicted values or labels.
+        cutoff : float, optional
+            Cutoff value to calculate precision and recall.
+        report : bool, optional
+            Whether to save JSON files of the resulting precision/recall metrics. Default is False.
+        output_dir : str, optional
+            Directory to save the output plots and report. Default is None.
 
-        Returns:
+        Raises
+        ------
+        ValueError
+            If `y_true` or `y_pred` is not provided.
+
+        Returns
+        -------
         None
         """
-
         if y_true is None or y_pred is None:
             raise ValueError("Must provide y_true and y_pred")
+        if cutoff is None:
+            raise ValueError("Must provide cutoff")
         self.plot_confusion_matrix(y_true, y_pred, cutoff, output_dir)
         self.plot_posthoc_classification(y_true, y_pred, cutoff, output_dir)
         precision, recall = self.get_precision_recall(y_pred, y_true, cutoff)
         self.report(report, output_dir, precision=precision, recall=recall)
 
-    def get_precision_recall(self, y_pred:list, y_true:list, cutoff:float):
+    def get_precision_recall(self, y_pred: list, y_true: list, cutoff: float):
         """
-        Calculate precision and recall metrics for given cutoffs.
+        Calculate precision and recall metrics for a given cutoff.
 
-        Parameters:
-        y_pred (array-like): Predicted values.
-        y_true (array-like): True values.
-        cutoff (float): Cutoff to calculate precision and recall.
+        Parameters
+        ----------
+        y_pred : array-like
+            Predicted values.
+        y_true : array-like
+            True values.
+        cutoff : float
+            Cutoff value to calculate precision and recall.
 
-        Returns:
-        Tuple: A tuple containing:
-            - precision (float): Precision value.
-            - recall (float): Recall value.
+        Returns
+        -------
+        tuple
+            A tuple containing:
+            - precision : float
+                Precision value.
+            - recall : float
+                Recall value.
         """
         pred_class = [y > cutoff for y in y_pred]
         true_class = [y > cutoff for y in y_true]
@@ -70,7 +91,7 @@ class PosthocBinaryMetrics(EvalBase):
         disp = ConfusionMatrixDisplay(cm)
         disp.plot()
         if output_dir is not None:
-            plt.savefig(f"{output_dir}/confusion_matrix.pdf")
+            plt.savefig(f"{output_dir}/confusion_matrix.png", dpi=300)        
 
     def plot_posthoc_classification(self, y_true:list, y_pred:list, cutoff:float, output_dir:str=None):
         """
@@ -82,9 +103,9 @@ class PosthocBinaryMetrics(EvalBase):
         plt.axhline(cutoff, color='r', linestyle='--')
         plt.xlabel("True Value")
         plt.ylabel("Predicted Value")
-        plt.title("Classification of the model")
+        plt.title(f"Post-hoc classification with cutoff: {cutoff} ")
         if output_dir is not None:
-            plt.savefig(f"{output_dir}/classification.pdf")
+            plt.savefig(f"{output_dir}/classification.png", dpi=300)
 
     def stats_to_json(self, data_df, output_dir):
         """
