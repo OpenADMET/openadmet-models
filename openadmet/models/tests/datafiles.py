784083296b0fa2adfdb21ad2e221888e36750033
hmacdope <hugomacdermott@gmail.com>
2025-05-20 11:54:52 +1000

    add test data and fix a small bug in chemprop

--- openadmet/models/tests/datafiles.py
+++ openadmet/models/tests/datafiles.py
@@ -39,6 +39,8 @@ ligand_pose = (_data_ref / "7kvh_A.pdb").as_posix()
 
 # stripped down anvil trained model directory for inference testing
 anvil_lgbm_trained_model_dir = (_data_ref / "cyp3a4_anvil_lgbm_model_dir").as_posix()
-
+anvil_chemprop_trained_model_dir = (_data_ref / "aceh_chemprop_anvil_model_dir").as_posix()
 # test data for prediction
 pred_test_data_csv = (_data_ref / "ligands.csv").as_posix()
+
+
--- openadmet/models/tests/inference/test_inference.py
+++ openadmet/models/tests/inference/test_inference.py
@@ -4,13 +4,27 @@ import pandas as pd
 import pytest
 
 from openadmet.models.inference.inference import predict
-from openadmet.models.tests.datafiles import pred_test_data_csv, anvil_lgbm_trained_model_dir
+from openadmet.models.tests.datafiles import pred_test_data_csv, anvil_lgbm_trained_model_dir, anvil_chemprop_trained_model_dir
 
-def test_predict():
+
+@pytest.fixture
+def anvil_lgbm():
+    return anvil_lgbm_trained_model_dir
+
+@pytest.fixture
+def anvil_chemprop():
+    return anvil_chemprop_trained_model_dir
+
+
+@pytest.mark.parametrize("model_dir", ["anvil_lgbm", "anvil_chemprop"])
+def test_predict(model_dir, request):
+
+    # Use the fixture to get the model directory
+    model_dir = request.getfixturevalue(model_dir)
     # Test the predict function with a sample input
     input_path = pred_test_data_csv
     input_col = "MY_SMILES"
-    model_dir = [anvil_lgbm_trained_model_dir]
+    model_dir = [model_dir]
     write_csv = False
     output_path = None
     debug = False
