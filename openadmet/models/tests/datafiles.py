9d885a0b5a678c36ecdef98d6ebabc4e1c82a29e
hmacdope <hugomacdermott@gmail.com>
2025-05-20 12:57:30 +1000

    add chemprop test

--- openadmet/models/tests/datafiles.py
+++ openadmet/models/tests/datafiles.py
@@ -28,6 +28,10 @@ AChE_CHEMBL4078_Landrum_maxcur = (
     _data_ref / "AChE_CHEMBL4078_Landrum_maxcur.csv"
 ).as_posix()
 
+ACEH_chembl_pchembl = (_data_ref / "ACEH_chembl_pchembl.csv").as_posix()
+acetylcholinesterase_anvil_chemprop_yaml = (_data_ref / "acetylcholinesterase_anvil_chemprop.yaml").as_posix()
+
+
 # posthoc anvil outputs for testing
 descr_json = _data_ref / "cross_validation_metrics_descr.json"
 fp_json = _data_ref / "cross_validation_metrics_fp.json"
