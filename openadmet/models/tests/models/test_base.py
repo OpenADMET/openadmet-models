f6ae106af4ec92ea275350beb57499f4100a841a
yxnyu <yx2892@nyu.edu>
2025-06-25 02:26:30 +0800

    fix the pytest

--- openadmet/models/tests/models/test_base.py
+++ openadmet/models/tests/models/test_base.py
@@ -1,10 +1,15 @@
 import pytest
 
-from openadmet.models.architecture.model_base import models
+from openadmet.models.architecture.model_base import (
+    PickleableModelBase,
+    models,
+)
 
 
-@pytest.mark.parametrize("mclass", models.classes())
+@pytest.mark.parametrize("mclass", models.values())
 def test_save_load_pickleable(mclass, tmp_path):
+    if not issubclass(mclass, PickleableModelBase):
+        pytest.skip(f"Skipping non-pickleable model {mclass.__name__}")
     model = mclass()
     model.build()
     model.save(tmp_path / "test_model.pkl")
