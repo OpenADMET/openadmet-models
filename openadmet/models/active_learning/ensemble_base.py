8a7aad92dc240a7ce87dd84e6bd33af4e2d1625f
Sean Colby <seanmcolby@gmail.com>
2025-07-29 15:37:54 -0800

    Add field validator for `n_models`

--- openadmet/models/active_learning/ensemble_base.py
+++ openadmet/models/active_learning/ensemble_base.py
@@ -1,6 +1,7 @@
 from typing import ClassVar
 
 from class_registry import ClassRegistry, RegistryKeyError
+from pydantic import field_validator
 
 from openadmet.models.architecture.model_base import ModelBase
 
@@ -25,7 +26,14 @@ class EnsembleBase(ModelBase):
 
     type: ClassVar[str] = "EnsembleBase"
     models: list = []
-    n_models: int = 0
+    n_models: int = 1
+
+    @field_validator("n_models")
+    @classmethod
+    def validate_n_models(cls, value):
+        if value < 1:
+            raise ValueError("Number of models must be greater than zero.")
+        return value
 
     def build(self):
         """
