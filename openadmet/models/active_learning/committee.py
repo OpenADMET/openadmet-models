2e7f5e74495e5adc4a4d38b093d6111707dc9669
Sean Colby <seanmcolby@gmail.com>
2025-08-05 15:48:25 -0800

    Disable loading for ensembles temporarily

--- openadmet/models/active_learning/committee.py
+++ openadmet/models/active_learning/committee.py
@@ -163,36 +163,39 @@ class CommitteeRegressor(EnsembleBase):
         for model, path in zip(self.models, paths):
             model.save(path)
 
-    @classmethod
-    def load(cls, paths: list[PathLike], model: ModelBase = None):
-        """
-        Load a committee model from the provided paths.
+    # @classmethod
+    # def load(cls, paths: list[PathLike], model: ModelBase = None):
+    #     """
+    #     Load a committee model from the provided paths.
 
-        Parameters
-        ----------
-        paths : list of PathLike
-            The file paths to the model weights.
-        model : ModelBase
-            Model type associated with path to weights.
+    #     Parameters
+    #     ----------
+    #     paths : list of PathLike
+    #         The file paths to the model weights.
+    #     model : ModelBase
+    #         Model type associated with path to weights.
 
-        Returns
-        -------
-        CommitteeRegressor
-            A committee model created from the loaded models.
+    #     Returns
+    #     -------
+    #     CommitteeRegressor
+    #         A committee model created from the loaded models.
 
-        """
+    #     """
 
-        # Check model type
-        if model is None:
-            raise ValueError("Must provide a model type to load.")
+    #     # Check model type
+    #     if model is None:
+    #         raise ValueError("Must provide a model type to load.")
 
-        # Load each model from the provided paths
-        models = []
-        for path in paths:
-            models.append(model.load(path))
+    #     # Load each model from the provided paths
+    #     models = []
+    #     for path in paths:
+    #         models.append(model.load(path))
 
-        # Create a CommitteeRegressor instance from the loaded models
-        return cls.from_models(models)
+    #     # Create a CommitteeRegressor instance from the loaded models
+    #     return cls.from_models(models)
+
+    def load(self, paths: list[PathLike], model: ModelBase = None):
+        pass
 
     def serialize(self, param_paths: list[PathLike], serial_paths: list[PathLike]):
         """
