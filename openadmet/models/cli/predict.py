ac7daf4b46f671ed71ee4037842576bf0b37d781
Sean Colby <seanmcolby@gmail.com>
2025-09-04 09:05:34 -0800

    Add initial acquisition function args

--- openadmet/models/cli/predict.py
+++ openadmet/models/cli/predict.py
@@ -1,6 +1,48 @@
 import click
 
+from openadmet.models.active_learning.acquisition import _QUERY_STRATEGIES
 from openadmet.models.inference.inference import predict as inference_func
+
+
+class AcquisitionGroup:
+    """Tracks acquisition function and its arguments."""
+
+    def __init__(self, name):
+        self.name = name
+        self.params = {}
+
+    def __repr__(self):
+        return f"{self.name}({self.params})"
+
+
+class AcquisitionParser(click.Option):
+    def __init__(self, *args, **kwargs):
+        self.seen_aqs = []
+        super().__init__(*args, **kwargs)
+
+    def handle_parse_result(self, ctx, opts, args):
+        # Reset global state for each parse
+        ctx.ensure_object(dict)
+        ctx.obj.setdefault("aq_groups", [])
+
+        aq_groups = ctx.obj["aq_groups"]
+
+        # Handle "--aq-fxn" specially
+        if self.name == "aq-fxn":
+            for aq in opts.get("aq-fxn", []):
+                aq_groups.append(AcquisitionGroup(aq))
+            opts.pop("aq-fxn", None)
+
+        # Handle parameters like beta, best-y, xi
+        for param in ("beta", "best_y", "xi"):
+            if param in opts:
+                values = opts.pop(param)
+                for val in values:
+                    if not aq_groups:
+                        raise click.UsageError(f"--{param} must follow an --aq option")
+                    aq_groups[-1].params[param] = val
+
+        return super().handle_parse_result(ctx, opts, args)
 
 
 @click.command()
@@ -33,13 +75,45 @@ from openadmet.models.inference.inference import predict as inference_func
 )
 @click.option(
     "--accelerator",
-    help="One of either cpu or gpu, default is gpu",
+    help="One of either cpu or gpu",
     required=False,
     default="gpu",
-    type=str,
+    type=click.Choice(["gpu", "cpu", "mls"], case_sensitive=False),
+    show_default=True,
+)
+# Acquisition function arguments
+@click.option(
+    "--aq-fxn",
+    type=click.Choice(_QUERY_STRATEGIES.keys(), case_sensitive=False),
+    multiple=True,
+    cls=AcquisitionParser,
+    help="Acquisition function",
+)
+@click.option(
+    "--beta",
+    type=float,
+    multiple=True,
+    cls=AcquisitionParser,
+    help="Tradeoff parameter (higher = more exploration). Used for UCB.",
+)
+@click.option(
+    "--best-y",
+    type=float,
+    multiple=True,
+    cls=AcquisitionParser,
+    help="Best observed value so far. Used for EI, PI.",
+)
+@click.option(
+    "--xi",
+    type=float,
+    multiple=True,
+    cls=AcquisitionParser,
+    help="Exploration-exploitation tradeoff parameter. Used for EI, PI.",
 )
 @click.option("--debug", is_flag=True, help="Enable debug mode", default=False)
-def predict(input_path, input_col, model_dir, output_csv, debug, accelerator):
+@click.pass_context
+def predict(ctx, input_path, input_col, model_dir, output_csv, debug, accelerator):
+    aq_groups = ctx.obj["aq_groups"]
     """Predict using a trained model"""
     inference_func(
         input_path=input_path,
