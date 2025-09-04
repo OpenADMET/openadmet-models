6491fe2520b7a9dd7216cba8455fa5eb8241437c
Sean Colby <seanmcolby@gmail.com>
2025-09-04 15:37:00 -0800

    Improved acquisition function argument parsing

--- openadmet/models/cli/predict.py
+++ openadmet/models/cli/predict.py
@@ -1,73 +1,31 @@
 import click
 
-from openadmet.models.active_learning.acquisition import _QUERY_STRATEGIES
 from openadmet.models.inference.inference import predict as inference_func
-
-
-class AcquisitionGroup:
-    """Tracks acquisition function and its arguments."""
-
-    def __init__(self, name):
-        self.name = name
-        self.params = {}
-
-    def __repr__(self):
-        return f"{self.name}({self.params})"
-
-
-class AcquisitionParser(click.Option):
-    def __init__(self, *args, **kwargs):
-        self.seen_aqs = []
-        super().__init__(*args, **kwargs)
-
-    def handle_parse_result(self, ctx, opts, args):
-        # Reset global state for each parse
-        ctx.ensure_object(dict)
-        ctx.obj.setdefault("aq_groups", [])
-
-        aq_groups = ctx.obj["aq_groups"]
-
-        # Handle "--aq-fxn" specially
-        if self.name == "aq-fxn":
-            for aq in opts.get("aq-fxn", []):
-                aq_groups.append(AcquisitionGroup(aq))
-            opts.pop("aq-fxn", None)
-
-        # Handle parameters like beta, best-y, xi
-        for param in ("beta", "best_y", "xi"):
-            if param in opts:
-                values = opts.pop(param)
-                for val in values:
-                    if not aq_groups:
-                        raise click.UsageError(f"--{param} must follow an --aq option")
-                    aq_groups[-1].params[param] = val
-
-        return super().handle_parse_result(ctx, opts, args)
 
 
 @click.command()
 @click.option(
     "--input-path",
-    help="Path to the input CSV file or SDF containing structures",
+    help="Path to the input CSV file or SDF containing structures.",
     required=True,
     type=click.Path(exists=True),
 )
 @click.option(
     "--input-col",
-    help="Column name in the CSV file containing input structure or OPENADMET_SMILES",
+    help="Column name in the CSV file containing input structure or OPENADMET_SMILES.",
     default="OPENADMET_SMILES",
     show_default=True,
 )
 @click.option(
     "--model-dir",
-    help="Path to a trained model directory as trained by `openadmet anvil`",
+    help="Path to a trained model directory as trained by `openadmet anvil`.",
     required=True,
     type=click.Path(exists=True),
     multiple=True,
 )
 @click.option(
     "--output-csv",
-    help="Path to the output CSV file for predictions",
+    help="Path to the output CSV file for predictions.",
     default="predictions.csv",
     show_default=True,
     required=True,
@@ -75,45 +33,35 @@ class AcquisitionParser(click.Option):
 )
 @click.option(
     "--accelerator",
-    help="One of either cpu or gpu",
+    help="Hardware to use for inference.",
     required=False,
     default="gpu",
     type=click.Choice(["gpu", "cpu", "mls"], case_sensitive=False),
     show_default=True,
 )
-# Acquisition function arguments
 @click.option(
     "--aq-fxn",
-    type=click.Choice(_QUERY_STRATEGIES.keys(), case_sensitive=False),
+    "aq_fxns",
+    type=click.Choice(["ucb", "ei", "pi"], case_sensitive=False),
     multiple=True,
-    cls=AcquisitionParser,
-    help="Acquisition function",
 )
-@click.option(
-    "--beta",
-    type=float,
-    multiple=True,
-    cls=AcquisitionParser,
-    help="Tradeoff parameter (higher = more exploration). Used for UCB.",
-)
-@click.option(
-    "--best-y",
-    type=float,
-    multiple=True,
-    cls=AcquisitionParser,
-    help="Best observed value so far. Used for EI, PI.",
-)
-@click.option(
-    "--xi",
-    type=float,
-    multiple=True,
-    cls=AcquisitionParser,
-    help="Exploration-exploitation tradeoff parameter. Used for EI, PI.",
-)
+@click.option("--beta", type=float, multiple=True)
+@click.option("--best-y", type=float, multiple=True)
+@click.option("--xi", type=float, multiple=True)
 @click.option("--debug", is_flag=True, help="Enable debug mode", default=False)
-@click.pass_context
-def predict(ctx, input_path, input_col, model_dir, output_csv, debug, accelerator):
-    aq_groups = ctx.obj["aq_groups"]
+def predict(
+    input_path,
+    input_col,
+    model_dir,
+    output_csv,
+    accelerator,
+    aq_fxns,
+    beta,
+    best_y,
+    xi,
+    debug,
+):
+    aq_fxn_args = _validate_aq_fxns(aq_fxns, beta, best_y, xi)
     """Predict using a trained model"""
     inference_func(
         input_path=input_path,
@@ -123,4 +71,42 @@ def predict(ctx, input_path, input_col, model_dir, output_csv, debug, accelerato
         output_csv=output_csv,
         debug=debug,
         accelerator=accelerator,
+        aq_fxn_args=aq_fxn_args,
     )
+
+
+def _validate_aq_fxns(aq_fxns, beta, best_y, xi):
+    # Tuple to list
+    aq_fxns = list(aq_fxns)
+    beta = list(beta)
+    best_y = list(best_y)
+    xi = list(xi)
+
+    # Check that each allowed acquisition function is specified at most once
+    if aq_fxns.count("ucb") > 1:
+        raise ValueError("UCB can only be specified once.")
+    if aq_fxns.count("ei") > 1:
+        raise ValueError("EI can only be specified once.")
+    if aq_fxns.count("pi") > 1:
+        raise ValueError("PI can only be specified once.")
+
+    # Check that `beta` is supplied the same number of times as ucb
+    if aq_fxns.count("ucb") != len(beta):
+        raise ValueError("Field `beta` must be specified for UCB acquisition.")
+
+    # Check that `best_y` and `xi` are supplied the same number of times as ei and pi combined
+    best_y_xi_count = aq_fxns.count("ei") + aq_fxns.count("pi")
+    if best_y_xi_count != len(best_y) or best_y_xi_count != len(xi):
+        raise ValueError(
+            "Fields `best_y` and `xi` must be specified once per EI and/or PI acquisition."
+        )
+
+    # Parse acquisition function arguments
+    result = {}
+    for aq_fxn in aq_fxns:
+        if aq_fxn == "ucb":
+            result[aq_fxn] = {"beta": beta.pop(0)}
+        elif aq_fxn in ["ei", "pi"]:
+            result[aq_fxn] = {"xi": xi.pop(0), "best_y": best_y.pop(0)}
+
+    return result
