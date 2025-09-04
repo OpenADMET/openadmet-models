02ef4cb9eb59b9ba67039a333ba4249fa7ed3a09
Devany West <devanywest314@gmail.com>
2025-09-04 17:14:58 -0400

    Rewrite Nepare module within openadmet framework

--- openadmet/models/architecture/nepare.py
+++ openadmet/models/architecture/nepare.py
@@ -4,13 +4,62 @@ from lightning import pytorch as pl
 from pydantic import field_validator
 from loguru import logger
 from chemprop import models, nn
+from typing import OrderedDict
 
 from openadmet.models.architecture.model_base import models as model_registry
 from openadmet.models.architecture.model_base import LightningModelBase
 
-from nepare.nn import NeuralPairwiseRegressor
 
 from typing import ClassVar
+
+class NeuralPairwiseRegressorModule(pl.LightningModule):
+    def __init__(self, 
+                 input_size, 
+                 hidden_size, 
+                 num_layers, 
+                 activation = torch.nn.ReLU, 
+                 lr: float = 1e-4, 
+                 n_targets: int = 1,
+                 monitor_metric: str = "val_loss"):
+        super().__init__()
+        input_size = input_size * 2
+        _modules = OrderedDict()
+        for i in range(num_layers):
+            _modules[f"hidden_{i}"] = torch.nn.Linear(input_size if i == 0 else hidden_size, hidden_size)
+            _modules[f"{activation.__name__.lower()}_{i}"] = activation()
+        _modules["readout"] = torch.nn.Linear(hidden_size, n_targets)
+        self.fnn = torch.nn.Sequential(_modules)
+        self.lr = lr
+        self.monitor_metric = monitor_metric
+        self.save_hyperparameters()
+
+    def configure_optimizers(self):
+        return torch.optim.AdamW(self.parameters(), self.lr)
+
+    def forward(self, x: torch.Tensor):
+        return self.fnn(x)
+
+    def training_step(self, batch: tuple[torch.Tensor, torch.Tensor], batch_idx):
+        return self._step(batch, "training")
+
+    def validation_step(self, batch: tuple[torch.Tensor, torch.Tensor], batch_idx):
+        return self._step(batch, "validation")
+
+    def test_step(self, batch: tuple[torch.Tensor, torch.Tensor], batch_idx):
+        return self._step(batch, "testing")
+    
+    def _step(self, batch: tuple[torch.Tensor, torch.Tensor, torch.Tensor], name: str):
+        x_1, x_2, y = batch
+        x = torch.cat((x_1, x_2), dim=1)
+        y_hat = self(x)
+        loss = torch.nn.functional.mse_loss(y_hat, y)
+        self.log(f"{name}/loss", loss, prog_bar=True)
+        return loss
+
+    def predict_step(self, batch: tuple[torch.Tensor, torch.Tensor, torch.Tensor]):
+        x_1, x_2, _ = batch
+        x = torch.cat((x_1, x_2), dim=1)
+        return self(x)
 
 @model_registry.register("NeuralPairwiseRegressorModel")
 class NeuralPairwiseRegressorModel(LightningModelBase):
@@ -21,7 +70,6 @@ class NeuralPairwiseRegressorModel(LightningModelBase):
 
     type: ClassVar[str] = "NeuralPairwiseRegressorModel"
     mod_params: dict = {}
-    monitor_metric: str = "val_loss"
 
     def train(self, dataloader):
         """
@@ -43,7 +91,7 @@ class NeuralPairwiseRegressorModel(LightningModelBase):
 
     def build(self, scaler=None):
         if not self.estimator:
-            nepare = NeuralPairwiseRegressor(**self.mod_params)
+            nepare = NeuralPairwiseRegressorModule(**self.mod_params)
             self.estimator = nepare
         else:
             logger.warning("Model already exists, skipping build")
