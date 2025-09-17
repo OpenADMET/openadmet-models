e0b8c50521e52f55f05fb669dd49f8226799e93d
Devany West <devanywest314@gmail.com>
2025-09-17 18:30:47 -0400

    Add parameter defs for docstrings in cli

--- openadmet/models/cli/predict.py
+++ openadmet/models/cli/predict.py
@@ -66,7 +66,33 @@ def predict(
     xi,
     debug,
 ):
-    """Predict using a trained model."""
+    """
+    Predict using a trained model.
+    
+    Parameters
+    ----------
+    input_path : str
+        Path to the input CSV file or SDF containing structures.
+    input_col : str
+        Column name in the CSV file containing input structure or OPENADMET_SMILES.
+    model_dir : str
+        Path to a trained model directory as trained by `openadmet anvil`.
+    output_csv : str
+        Path to the output CSV file for predictions.
+    accelerator : str
+        Hardware to use for inference.
+    aq_fxns : list
+        Acquisition function(s) to use for active learning.
+    beta : list
+        Beta values for UCB acquisition function.
+    best_y : list
+        Best observed values for EI and PI acquisition functions.
+    xi : list
+        Exploration parameter for EI and PI acquisition functions.
+    debug : bool
+        Enable debug mode.
+    
+    """
     aq_fxn_args = _validate_aq_fxns(aq_fxns, beta, best_y, xi)
     inference_func(
         input_path=input_path,
