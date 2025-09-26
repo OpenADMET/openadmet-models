6198e6b59c0596d76244ddeb19a74effe8cc6843
hmacdope <hugomacdermott@gmail.com>
2025-09-26 11:21:24 +1000

    add jupyter guard to notebook

--- openadmet/models/log.py
+++ openadmet/models/log.py
@@ -2,5 +2,20 @@
 
 from loguru import logger
 from rich.logging import RichHandler
+import sys
 
-logger.configure(handlers=[{"sink": RichHandler(), "format": "{message}"}])
+
+
+def is_notebook() -> bool:
+    try:
+        get_ipython  # type: ignore
+        return True
+    except NameError:
+        return False
+
+
+if not is_notebook():
+    from rich.logging import RichHandler
+    logger.configure(
+        handlers=[{"sink": RichHandler(), "format": "{message}"}]
+    )
